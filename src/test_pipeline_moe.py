254ca10a00631298d6fbd716e2822b6e45bbe470
Arnoochka <kolos.130278@gmail.com>
2026-05-06 19:29:55 +0000

    add moe-base

--- src/test_pipeline_moe.py
+++ src/test_pipeline_moe.py
@@ -83,7 +83,7 @@ def start(prompts: List[str],
     dtype=torch.bfloat16,
     save_model_config=False,
     save_stats=True,
-    save_dir=f"results/base_test/pipe/batch_size={batch_size}-prompt_len={max_prompt_len}-new_tokens={max_new_tokens}-num_microbatch={num_microbatches}-")
+    save_dir=f"results/moe/base_test/moe-base/batch_size={batch_size}-prompt_len={max_prompt_len}-new_tokens={max_new_tokens}-num_microbatch={num_microbatches}-")
     stats = benchmark(
     prompts=prompts,
     batch_size=batch_size // num_microbatches,
